220b7069e1b3376e416d7f0cc8df45098bbba0b3
Mathias STRASSER <mathias.strasser@shippeo.com>
2018-10-16 12:06:28 +0200

    Extract method to convert into unicode

--- src/Message/EncodingDetector.php
+++ src/Message/EncodingDetector.php
@@ -13,13 +13,7 @@ class EncodingDetector {
     public function requiresUnicodeEncoding($content)
     {
         $gsmCodePoints = array_map(
-            function ($char) {
-                $k = mb_convert_encoding($char, 'UTF-16LE', 'UTF-8');
-                $k1 = ord(substr($k, 0, 1));
-                $k2 = ord(substr($k, 1, 1));
-
-                return $k2 * 256 + $k1;
-            },
+            $this->convertIntoUnicode(),
             [ // See: https://en.wikipedia.org/wiki/GSM_03.38#GSM_7-bit_default_alphabet_and_extension_table_of_3GPP_TS_23.038_/_GSM_03.38
                 '@', '£', '$', '¥', 'è', 'é', 'ù', 'ì', 'ò', 'ç', "\r", 'Ø', 'ø', "\n", 'Å', 'å',
                 'Δ', '_', 'Φ', 'Γ', 'Λ', 'Ω', 'Π', 'Ψ', 'Σ', 'Θ', 'Ξ', 'Æ', 'æ', 'ß', 'É',
@@ -37,17 +31,23 @@ class EncodingDetector {
         $textChars = preg_split('//u', $content, null, PREG_SPLIT_NO_EMPTY);
 
         // Array of codepoint values for characters in $text.
-        $textCodePoints = array_map(function ($char) {
-            $k = mb_convert_encoding($char, 'UTF-16LE', 'UTF-8');
-            $k1 = ord(substr($k, 0, 1));
-            $k2 = ord(substr($k, 1, 1));
-            return $k2 * 256 + $k1;
-        }, $textChars);
+        $textCodePoints = array_map($this->convertIntoUnicode(), $textChars);
 
         // Filter the array to contain only codepoints from $text that are not in the set of valid GSM codepoints.
         $nonGsmCodePoints = array_diff($textCodePoints, $gsmCodePoints);
 
         // The text contains unicode if the result is not empty.
         return !empty($nonGsmCodePoints);
+    }
+
+    private function convertIntoUnicode()
+    {
+        return function ($char) {
+            $k = mb_convert_encoding($char, 'UTF-16LE', 'UTF-8');
+            $k1 = ord(substr($k, 0, 1));
+            $k2 = ord(substr($k, 1, 1));
+
+            return $k2 * 256 + $k1;
+        };
     }
 }
