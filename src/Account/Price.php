15ab5fd97b3826f7fb39b3b2c81607a78d946fd4
Ali Yousefi <ayousefi@outlook.com>
2020-04-10 13:22:24 +0430

    refactor Account/Price.php - use look up table and change if condition syntax

--- src/Account/Price.php
+++ src/Account/Price.php
@@ -20,28 +20,23 @@ abstract class Price implements EntityInterface, JsonSerializableInterface, Json
 
     protected $data = [];
 
-    public function getCountryCode()
-    {
+    public function getCountryCode() {
         return $this['country_code'];
     }
 
-    public function getCountryDisplayName()
-    {
+    public function getCountryDisplayName() {
         return $this['country_display_name'];
     }
 
-    public function getCountryName()
-    {
+    public function getCountryName() {
         return $this['country_name'];
     }
 
-    public function getDialingPrefix()
-    {
+    public function getDialingPrefix() {
         return $this['dialing_prefix'];
     }
 
-    public function getDefaultPrice()
-    {
+    public function getDefaultPrice() {
         if (isset($this['default_price'])) {
             return $this['default_price'];
         }
@@ -49,18 +44,15 @@ abstract class Price implements EntityInterface, JsonSerializableInterface, Json
         return $this['mt'];
     }
 
-    public function getCurrency()
-    {
+    public function getCurrency() {
         return $this['currency'];
     }
 
-    public function getNetworks()
-    {
+    public function getNetworks() {
         return $this['networks'];
     }
 
-    public function getPriceForNetwork($networkCode)
-    {
+    public function getPriceForNetwork($networkCode) {
         $networks = $this->getNetworks();
         if (isset($networks[$networkCode])) {
             return $networks[$networkCode]->{$this->priceMethod}();
@@ -69,25 +61,24 @@ abstract class Price implements EntityInterface, JsonSerializableInterface, Json
         return $this->getDefaultPrice();
     }
 
-    public function jsonUnserialize(array $json)
-    {
+    public function jsonUnserialize(array $json) {
         // Convert CamelCase to snake_case as that's how we use array access in every other object
         $data = [];
+
+        $map = [
+            'country' => 'country_code',
+            'prefix' => 'dialing_prefix'
+        ];
+
         foreach ($json as $k => $v) {
             $k = ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $k)), '_');
 
             // PrefixPrice fixes
-            if ($k == 'country') {
-                $k = 'country_code';
-            }
+            $k = $map[$k];
 
             if ($k == 'name') {
                 $data['country_display_name'] = $v;
                 $data['country_name'] = $v;
-            }
-
-            if ($k == 'prefix') {
-                $k = 'dialing_prefix';
             }
 
             $data[$k] = $v;
@@ -95,50 +86,46 @@ abstract class Price implements EntityInterface, JsonSerializableInterface, Json
 
         // Create objects for all the nested networks too
         $networks = [];
-        if (isset($json['networks'])) {
-            foreach ($json['networks'] as $n) {
-                if (isset($n['code'])) {
-                    $n['networkCode'] = $n['code'];
-                    unset($n['code']);
-                }
-
-                if (isset($n['network'])) {
-                    $n['networkName'] = $n['network'];
-                    unset($n['network']);
-                }
-
-                $network = new Network($n['networkCode'], $n['networkName']);
-                $network->jsonUnserialize($n);
-                $networks[$network->getCode()] = $network;
+        if (!isset($json['networks'])) {
+            $this->data = $data;
+        }
+        foreach ($json['networks'] as $n) {
+            if (isset($n['code'])) {
+                $n['networkCode'] = $n['code'];
+                unset($n['code']);
             }
+
+            if (isset($n['network'])) {
+                $n['networkName'] = $n['network'];
+                unset($n['network']);
+            }
+
+            $network = new Network($n['networkCode'], $n['networkName']);
+            $network->jsonUnserialize($n);
+            $networks[$network->getCode()] = $network;
         }
 
         $data['networks'] = $networks;
         $this->data = $data;
     }
 
-    public function jsonSerialize()
-    {
+    public function jsonSerialize() {
         return $this->data;
     }
 
-    public function offsetExists($offset)
-    {
+    public function offsetExists($offset) {
         return isset($this->data[$offset]);
     }
 
-    public function offsetGet($offset)
-    {
+    public function offsetGet($offset) {
         return $this->data[$offset];
     }
 
-    public function offsetSet($offset, $value)
-    {
+    public function offsetSet($offset, $value) {
         throw new Exception('Price is read only');
     }
 
-    public function offsetUnset($offset)
-    {
+    public function offsetUnset($offset) {
         throw new Exception('Price is read only');
     }
 }
