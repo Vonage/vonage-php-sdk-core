88cea8e4c707d41eca9e6a7808112a690116858b
Chris Tankersley <chris@ctankersley.com>
2021-02-23 09:41:18 -0500

    Added ability to set DLT information (#273)

--- src/SMS/Message/SMS.php
+++ src/SMS/Message/SMS.php
@@ -20,6 +20,16 @@ class SMS extends OutboundMessage
     /**
      * @var string
      */
+    protected $contentId;
+
+    /**
+     * @var string
+     */
+    protected $entityId;
+
+    /**
+     * @var string
+     */
     protected $message;
 
     /**
@@ -38,12 +48,42 @@ class SMS extends OutboundMessage
         $this->message = $message;
     }
 
+    public function getContentId(): string
+    {
+        return $this->contentId;
+    }
+
+    public function getEntityId(): string
+    {
+        return $this->entityId;
+    }
+
+    public function setContentId(string $id): self
+    {
+        $this->contentId = $id;
+        return $this;
+    }
+
+    public function setEntityId(string $id): self
+    {
+        $this->entityId = $id;
+        return $this;
+    }
+
     /**
-     * @return mixed
+     * @return array<mixed>
      */
     public function toArray(): array
     {
         $data = ['text' => $this->getMessage()];
+        if (!empty($this->entityId)) {
+            $data['entity-id'] = $this->entityId;
+        }
+
+        if (!empty($this->contentId)) {
+            $data['content-id'] = $this->contentId;
+        }
+
         $data = $this->appendUniversalOptions($data);
 
         return $data;
@@ -52,5 +92,13 @@ class SMS extends OutboundMessage
     public function getMessage(): string
     {
         return $this->message;
+    }
+
+    public function enableDLT(string $entityId, string $templateId): self
+    {
+        $this->entityId = $entityId;
+        $this->contentId = $templateId;
+
+        return $this;
     }
 }
