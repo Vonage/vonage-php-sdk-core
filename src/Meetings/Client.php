377b9472bad08ba9a33fe92b9ee100f7646f9e79
James Seconde <jim.seconde@googlemail.com>
2023-01-17 12:06:15 +0000

    Meetings base client

---
+++ src/Meetings/Client.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Vonage Client Library for PHP
+ *
+ * @copyright Copyright (c) 2016-2022 Vonage, Inc. (http://vonage.com)
+ * @license https://github.com/Vonage/vonage-php-sdk-core/blob/master/LICENSE.txt Apache License 2.0
+ */
+
+declare(strict_types=1);
+
+namespace Vonage\Meetings;
+
+use Vonage\Client\APIClient;
+use Vonage\Client\APIResource;
+
+class Client implements APIClient
+{
+    public function __construct(protected APIResource $api)
+    {
+    }
+
+    public function getAPIResource(): APIResource
+    {
+        return $this->api;
+    }
+}
---
+++ src/Meetings/ClientFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Vonage Client Library for PHP
+ *
+ * @copyright Copyright (c) 2016-2022 Vonage, Inc. (http://vonage.com)
+ * @license https://github.com/Vonage/vonage-php-sdk-core/blob/master/LICENSE.txt Apache License 2.0
+ */
+
+declare(strict_types=1);
+
+namespace Vonage\Meetings;
+
+use Psr\Container\ContainerInterface;
+use Vonage\Client\APIResource;
+use Vonage\Client\Credentials\Handler\KeypairHandler;
+
+class ClientFactory
+{
+    public function __invoke(ContainerInterface $container): Client
+    {
+        /** @var APIResource $api */
+        $api = $container->make(APIResource::class);
+        $api
+            ->setBaseUrl('https://api-eu.vonage.com/beta/meetings/')
+            ->setIsHAL(true)
+            ->setAuthHandler(new KeypairHandler());
+
+        return new Client($api);
+    }
+}
