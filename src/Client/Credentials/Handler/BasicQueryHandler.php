7735ca5127259c24808d15a7d2b08171610b73bf
James Seconde <jim.seconde@googlemail.com>
2023-02-28 15:45:33 +0000

    Forgot to add return value to request (#380)
    * Forgot to add return value to request
    
    * Fix the basic query handler

--- src/Client/Credentials/Handler/BasicQueryHandler.php
+++ src/Client/Credentials/Handler/BasicQueryHandler.php
@@ -11,7 +11,9 @@ class BasicQueryHandler extends AbstractHandler
     public function __invoke(RequestInterface $request, CredentialsInterface $credentials): RequestInterface
     {
         $credentials = $this->extract(Basic::class, $credentials);
+        parse_str($request->getUri()->getQuery(), $query);
+        $query = array_merge($query, $credentials->asArray());
 
-        return $request->withUri($request->getUri()->withQuery(http_build_query($credentials->asArray())));
+        return $request->withUri($request->getUri()->withQuery(http_build_query($query)));
     }
 }
