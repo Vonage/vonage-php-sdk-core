f13946eefd68ee3dd506e74005206cc141202d74
James Seconde <jim.seconde@googlemail.com>
2023-03-29 09:52:18 +0100

    changed logic for webhook

---
+++ src/Verify2/VerifyObjects/VerifyWhatsAppInteractiveUpdate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vonage\Verify2\VerifyObjects;
+
+use Vonage\Entity\Hydrator\ArrayHydrateInterface;
+
+class VerifyWhatsAppInteractiveUpdate implements ArrayHydrateInterface
+{
+    private array $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function __get($property)
+    {
+        return $this->data[$property] ?? null;
+    }
+
+    public function __set($property, $value)
+    {
+        $this->data[$property] = $value;
+
+        return $this;
+    }
+
+    public function __isset(string $name): bool
+    {
+        return isset($this->data[$name]);
+    }
+
+    public function fromArray(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return $this->data;
+    }
+}
--- src/Verify2/Webhook/Factory.php
+++ src/Verify2/Webhook/Factory.php
@@ -5,6 +5,7 @@ namespace Vonage\Verify2\Webhook;
 use Vonage\Verify2\VerifyObjects\VerifyEvent;
 use Vonage\Verify2\VerifyObjects\VerifySilentAuthUpdate;
 use Vonage\Verify2\VerifyObjects\VerifyStatusUpdate;
+use Vonage\Verify2\VerifyObjects\VerifyWhatsAppInteractiveUpdate;
 
 class Factory extends \Vonage\Webhook\Factory
 {
@@ -12,18 +13,19 @@ class Factory extends \Vonage\Webhook\Factory
      * Warning: This logic is fairly brittle, since there are no current better ways of determining
      * the type of event or update.
      */
-    public static function createFromArray(array $data): VerifyStatusUpdate|VerifySilentAuthUpdate|VerifyEvent
+    public static function createFromArray(array $data): VerifyWhatsAppInteractiveUpdate|VerifyStatusUpdate|VerifySilentAuthUpdate
     {
-        if (array_key_exists('action', $data)) {
-            return new VerifySilentAuthUpdate($data);
+        if ($data['type'] === 'event') {
+            if ($data['channel'] === 'silent_auth') {
+                return new VerifySilentAuthUpdate($data);
+            }
+            if ($data['channel'] === 'whatsapp_interactive') {
+                return new VerifyWhatsAppInteractiveUpdate($data);
+            }
         }
 
-        if (array_key_exists('price', $data)) {
+        if ($data['type'] === 'summary') {
             return new VerifyStatusUpdate($data);
-        }
-
-        if (array_key_exists('triggered_at', $data)) {
-            return new VerifyEvent($data);
         }
 
         throw new \OutOfBoundsException('Could not create Verify2 Object from payload');
