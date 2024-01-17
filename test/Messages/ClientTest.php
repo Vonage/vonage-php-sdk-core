5e77b30661709a45627639c3e1e3e6ac2451a77b
James Seconde <jim.seconde@googlemail.com>
2024-01-17 15:15:43 +0000

    New fields added

--- test/Messages/ClientTest.php
+++ test/Messages/ClientTest.php
@@ -122,6 +122,49 @@ class ClientTest extends VonageTestCase
         $this->assertArrayHasKey('message_uuid', $result);
     }
 
+    public function testCanSendSMSWithOptionalFields(): void
+    {
+        $payload = [
+            'to' => '447700900000',
+            'from' => '16105551212',
+            'text' => 'Reticulating Splines',
+            'encoding_type' => 'auto',
+            'content_id' => '1107457532145798767',
+            'entity_id' => '1101456324675322134',
+            'webhook_url' => 'https://example.com/status',
+            'webhook_version' => 'v1',
+            'ttl' => 300
+        ];
+
+        $message = new SMSText($payload['to'], $payload['from'], $payload['text']);
+        $message->setEncodingType($payload['encoding_type']);
+        $message->setTtl($payload['ttl']);
+        $message->setContentId($payload['content_id']);
+        $message->setEntityId($payload['entity_id']);
+        $message->setWebhookUrl($payload['webhook_url']);
+        $message->setWebhookVersion($payload['webhook_version']);
+
+        $this->vonageClient->send(Argument::that(function (Request $request) use ($payload) {
+            $this->assertRequestJsonBodyContains('ttl', $payload['ttl'], $request);
+            $this->assertRequestJsonBodyContains('webhook_url', $payload['webhook_url'], $request);
+            $this->assertRequestJsonBodyContains('webhook_version', $payload['webhook_version'], $request);
+            $smsObject = [
+                'encoding_type' => $payload['encoding_type'],
+                'content_id' => $payload['content_id'],
+                'entity_id' => $payload['entity_id']
+            ];
+
+            $this->assertRequestJsonBodyContains('sms', $smsObject, $request);
+
+            $this->assertEquals('POST', $request->getMethod());
+
+            return true;
+        }))->willReturn($this->getResponse('sms-success', 202));
+        $result = $this->messageClient->send($message);
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('message_uuid', $result);
+    }
+
     public function testCanSendMMSImage(): void
     {
         $imageUrl = 'https://picsum.photos/200/300';
