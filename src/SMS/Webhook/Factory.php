0bca932cd2c4de1ff532ad3a020a4ec8f51b244d
James Seconde <jim.seconde@googlemail.com>
2024-04-12 15:11:28 +0100

    Remove copyright noise that is covered by the Apache 2.0 licence.

--- src/SMS/Webhook/Factory.php
+++ src/SMS/Webhook/Factory.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Vonage Client Library for PHP
- *
- * @copyright Copyright (c) 2016-2022 Vonage, Inc. (http://vonage.com)
- * @license https://github.com/Vonage/vonage-php-sdk-core/blob/master/LICENSE.txt Apache License 2.0
- */
-
 declare(strict_types=1);
 
 namespace Vonage\SMS\Webhook;
