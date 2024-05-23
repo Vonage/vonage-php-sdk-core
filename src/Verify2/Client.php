f1bc731337896574cf130883370a89df0c63868e
James Seconde <jim.seconde@googlemail.com>
2024-05-23 15:36:34 +0100

    Add next workflow to SDK (#476)

--- src/Verify2/Client.php
+++ src/Verify2/Client.php
@@ -5,6 +5,7 @@ namespace Vonage\Verify2;
 use Vonage\Client\APIClient;
 use Vonage\Client\APIResource;
 use Vonage\Client\Exception\Exception;
+use Vonage\Client\Exception\Request;
 use Vonage\Verify2\Request\BaseVerifyRequest;
 
 class Client implements APIClient
@@ -30,7 +31,7 @@ class Client implements APIClient
         } catch (Exception $e) {
             // For horrible reasons in the API Error Handler, throw the error unless it's a 409.
             if ($e->getCode() === 409) {
-                throw new \Vonage\Client\Exception\Request('Conflict: The current Verify workflow step does not support a code.');
+                throw new Request('Conflict: The current Verify workflow step does not support a code.');
             }
 
             throw $e;
@@ -42,6 +43,13 @@ class Client implements APIClient
     public function cancelRequest(string $requestId): bool
     {
         $this->api->delete($requestId);
+
+        return true;
+    }
+
+    public function nextWorkflow(string $requestId): bool
+    {
+        $this->api->create([], '/' . $requestId . '/next_workflow');
 
         return true;
     }
