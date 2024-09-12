6b3a8b7896051b7401004794814ef10486c60913
James Seconde <jim.seconde@googlemail.com>
2024-09-12 10:45:53 +0100

    correct the nullcheck on whatsapp context. (#500)
    * correct the nullcheck on whatsapp context.
    
    * recorrect the type check because chatGPT lies to me. Add test.

--- src/Messages/Channel/WhatsApp/WhatsAppText.php
+++ src/Messages/Channel/WhatsApp/WhatsAppText.php
@@ -28,10 +28,7 @@ class WhatsAppText extends BaseMessage
     {
         $returnArray = $this->getBaseMessageUniversalOutputArray();
         $returnArray['text'] = $this->getText();
-
-        if (!is_null($this->context)) {
-            $returnArray['context'] = $this->context;
-        }
+        $returnArray['context'] = $this->context ?? null;
 
         return $returnArray;
     }
