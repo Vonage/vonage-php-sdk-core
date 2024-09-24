dac3ac938a8e98eeaac4bca31b5c7670a9828f4c
Jim Seconde <jim.seconde@googlemail.com>
2024-09-24 15:43:12 +0100

    Credentials typehint cleaning

--- src/Client/Credentials/Basic.php
+++ src/Client/Credentials/Basic.php
@@ -7,17 +7,11 @@ namespace Vonage\Client\Credentials;
 /**
  * Class Basic
  * Read-only container for api key and secret.
- *
- * @property string api_key
- * @property string api_secret
  */
 class Basic extends AbstractCredentials
 {
     /**
      * Create a credential set with an API key and secret.
-     *
-     * @param $key
-     * @param $secret
      */
     public function __construct($key, $secret)
     {
