46bfd5c358ca87002cbd7db3d6f27d9da347cb29
Jim Seconde <jim.seconde@googlemail.com>
2024-12-04 13:37:43 +0000

    phpstan checks

--- src/Client/Exception/Validation.php
+++ src/Client/Exception/Validation.php
@@ -11,7 +11,7 @@ class Validation extends Request
     public function __construct(
         string $message = '',
         int $code = 0,
-        Throwable $previous = null,
+        ?Throwable $previous = null,
         private readonly array $errors = []
     ) {
         parent::__construct($message, $code, $previous);
